5888c60eecc2333a693aa0631ffbcd4a154b1892
mthunz <fakemggg@gmaill.com>
2024-08-26 21:52:10 +0700

    auto

--- handlers/connection.js
+++ handlers/connection.js
@@ -13,7 +13,10 @@ module.exports = {
             return new Promise((resolve, reject) => {
                 const start = Date.now()
 
-                const redis = new Redis(redisPort);
+                const redis = new Redis({
+                    host: '172.17.0.2',
+                    port: redisPort
+                });
 
                 redis.on('connected', () => {
                     log('Redis', `&aConnected to port &d${redisPort} &f[&b${Date.now() - start}ms&f]`)
